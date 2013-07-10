d2283f557ae5fb9511c99777970d7361f3603d6b
Nathan Spoonts <nspoonts@gmail.com>
2013-07-10 16:37:32 -0500

    Updated web.js

--- web.js
+++ web.js
@@ -3,7 +3,9 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  buf = new Buffer(256);
+  text = fs.readFileSync("index.html");
+  response.send(buf.toString(text));
 });
 
 var port = process.env.PORT || 5000;
